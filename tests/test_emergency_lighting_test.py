be295d59164d6d313016479dd2e569cad66ddf31
Michal Erazmus <michal.erazmus@silvair.com>
2024-02-01 20:56:39 +0100

    SP-NONE: Fix Emergency Lighting Test, add more tests

--- tests/test_emergency_lighting_test.py
+++ tests/test_emergency_lighting_test.py
@@ -2,9 +2,9 @@ from datetime import datetime, timedelta, timezone
 
 import pytest
 
-from bluetooth_mesh.messages.properties import PropertyID
 from bluetooth_mesh.messages.silvair.emergency_lighting_test import (
     EmergencyLightingTestParams,
+    EmergencyLightingTestProperty,
     EmergencyLightingTestSubOpcode,
     TestExecutionStatus,
 )
@@ -145,34 +145,251 @@ valid = [
         ),
     ),
     pytest.param(
+        bytes.fromhex("0884FF"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_GET,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_EXECUTION_TIMEOUT,
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0885FF"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_GET,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_EXECUTION_TIMEOUT,
+        ),
+    ),
+    pytest.param(
         bytes.fromhex("0886FF"),
         EmergencyLightingTestSubOpcode.ELT_PROPERTY_GET,
         dict(
-            property_id=PropertyID.ELT_DURATION_TEST_RETRY_PERIOD,
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_RETRY_PERIOD,
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0887FF"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_GET,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_RETRY_PERIOD,
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0888FF"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_GET,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_BACKUP_AUTOMATIC_DELAY,
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0889FF"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_GET,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_BACKUP_AUTOMATIC_DELAY,
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("088AFF"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_GET,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_BACKUP_AUTOMATIC_INTERVAL,
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("088BFF"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_GET,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_BACKUP_AUTOMATIC_INTERVAL,
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0984FF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_EXECUTION_TIMEOUT,
+            elt_duration_test_execution_timeout=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0985FF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_EXECUTION_TIMEOUT,
+            elt_functional_test_execution_timeout=dict(seconds=0x01020304),
         ),
     ),
     pytest.param(
         bytes.fromhex("0986FF04030201"),
         EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET,
         dict(
-            property_id=PropertyID.ELT_DURATION_TEST_RETRY_PERIOD,
-            value=dict(seconds=0x01020304),
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_RETRY_PERIOD,
+            elt_duration_test_retry_period=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0987FF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_RETRY_PERIOD,
+            elt_functional_test_retry_period=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0988FF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_BACKUP_AUTOMATIC_DELAY,
+            elt_duration_test_backup_automatic_delay=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0989FF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_BACKUP_AUTOMATIC_DELAY,
+            elt_functional_test_backup_automatic_delay=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("098AFF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_BACKUP_AUTOMATIC_INTERVAL,
+            elt_duration_test_backup_automatic_interval=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("098BFF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_BACKUP_AUTOMATIC_INTERVAL,
+            elt_functional_test_backup_automatic_interval=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0A84FF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET_UNACKNOWLEDGED,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_EXECUTION_TIMEOUT,
+            elt_duration_test_execution_timeout=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0A85FF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET_UNACKNOWLEDGED,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_EXECUTION_TIMEOUT,
+            elt_functional_test_execution_timeout=dict(seconds=0x01020304),
         ),
     ),
     pytest.param(
         bytes.fromhex("0A86FF04030201"),
         EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET_UNACKNOWLEDGED,
         dict(
-            property_id=PropertyID.ELT_DURATION_TEST_RETRY_PERIOD,
-            value=dict(seconds=0x01020304),
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_RETRY_PERIOD,
+            elt_duration_test_retry_period=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0A87FF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET_UNACKNOWLEDGED,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_RETRY_PERIOD,
+            elt_functional_test_retry_period=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0A88FF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET_UNACKNOWLEDGED,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_BACKUP_AUTOMATIC_DELAY,
+            elt_duration_test_backup_automatic_delay=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0A89FF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET_UNACKNOWLEDGED,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_BACKUP_AUTOMATIC_DELAY,
+            elt_functional_test_backup_automatic_delay=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0A8AFF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET_UNACKNOWLEDGED,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_BACKUP_AUTOMATIC_INTERVAL,
+            elt_duration_test_backup_automatic_interval=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0A8BFF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_SET_UNACKNOWLEDGED,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_BACKUP_AUTOMATIC_INTERVAL,
+            elt_functional_test_backup_automatic_interval=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0B84FF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_STATUS,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_EXECUTION_TIMEOUT,
+            elt_duration_test_execution_timeout=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0B85FF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_STATUS,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_EXECUTION_TIMEOUT,
+            elt_functional_test_execution_timeout=dict(seconds=0x01020304),
         ),
     ),
     pytest.param(
         bytes.fromhex("0B86FF04030201"),
         EmergencyLightingTestSubOpcode.ELT_PROPERTY_STATUS,
         dict(
-            property_id=PropertyID.ELT_DURATION_TEST_RETRY_PERIOD,
-            value=dict(seconds=0x01020304),
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_RETRY_PERIOD,
+            elt_duration_test_retry_period=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0B87FF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_STATUS,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_RETRY_PERIOD,
+            elt_functional_test_retry_period=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0B88FF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_STATUS,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_BACKUP_AUTOMATIC_DELAY,
+            elt_duration_test_backup_automatic_delay=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0B89FF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_STATUS,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_BACKUP_AUTOMATIC_DELAY,
+            elt_functional_test_backup_automatic_delay=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0B8AFF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_STATUS,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_DURATION_TEST_BACKUP_AUTOMATIC_INTERVAL,
+            elt_duration_test_backup_automatic_interval=dict(seconds=0x01020304),
+        ),
+    ),
+    pytest.param(
+        bytes.fromhex("0B8BFF04030201"),
+        EmergencyLightingTestSubOpcode.ELT_PROPERTY_STATUS,
+        dict(
+            property_id=EmergencyLightingTestProperty.ELT_FUNCTIONAL_TEST_BACKUP_AUTOMATIC_INTERVAL,
+            elt_functional_test_backup_automatic_interval=dict(seconds=0x01020304),
         ),
     ),
 ]
